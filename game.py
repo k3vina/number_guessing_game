9b5e91d11b77d30efa58f94c9294934d308b8bb6
k3vina <kevinajepkonga@gmail.com>
2026-08-14 10:07:45 +0300

    game.py

--- game.py
+++ game.py
@@ -1,9 +1,8 @@
 import random
 
-number = random.randint(1, 100)
 
 def user_input():
-    print("Welcom to the number guessing game!")
+    print("Welcome to the number guessing game!")
     print("Choose a diffculty.")
 
     difficulty_level = {
@@ -28,5 +27,29 @@ def user_input():
                 print("Ivalid choice. Please enter 1, 2, or 3.")
         except ValueError:
             print(f"That's not a number. Please enter 1, 2, or 3")
-            
-user_input()
+
+
+def give_feedback(number, tries):
+
+    
+
+    while tries > 0:
+        guess = int(input("Guess a number between 1 and 100: "))
+
+        if guess == number:
+            print("Congratulations! You have won!")
+            return 
+        elif guess < number:
+            print("Too low")
+        else:
+            print("Too high")
+
+        tries -= 1
+        print(f"You have {tries} tries remaining")
+
+    if tries == 0:
+        print(f"You have lost.The number is {number}.")
+
+number = random.randint(1, 100)
+tries = user_input()
+give_feedback(number, tries)
